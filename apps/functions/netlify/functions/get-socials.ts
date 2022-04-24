8535c16a8a8b551e001e74a55b99a04673486f74
christaylor19 <christaylormail.94@gmail.com>
2022-04-24 17:49:27 +0100

    Add further logs

--- apps/functions/netlify/functions/get-socials.ts
+++ apps/functions/netlify/functions/get-socials.ts
@@ -28,12 +28,13 @@ const handler: Handler = async (event, context) => {
 
   try {
     const data = getSocials(social as Social);
-
+    console.log('Data: ', data);
     return {
       statusCode: 200,
       body: JSON.stringify({ data: data, error: false }),
     };
   } catch (error) {
+    console.log('errpr: ', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
