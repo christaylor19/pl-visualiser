9ac777891fff691dda337d7908b6cfca4f03cbd5
christaylor19 <christaylormail.94@gmail.com>
2022-04-24 19:08:40 +0100

    Create logger package

--- apps/site/src/pages/Home.tsx
+++ apps/site/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { log } from 'logger';
 import { FC, useContext } from 'react';
 import styled from 'styled-components';
 
@@ -32,6 +33,8 @@ const Row = styled.div`
 
 const Home: FC = () => {
   const { mode } = useContext(ModeContext);
+
+  log(mode)
 
   return (
     <Layout>
